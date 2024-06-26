d0a385004b50635a697c88450eb6dff5703ecc42
Jacob Praskala <praskalaj@gmail.com>
2024-06-26 14:40:15 -0400

    Making Progress

--- ml/cpp_files/data.cpp
+++ ml/cpp_files/data.cpp
@@ -1,2 +1,49 @@
 #include "data.h"
 
+DataCollection::DataCollection() {
+    csvPath = "../../wii.csv";
+    numRows = 397;
+    numColumns = 6;
+}
+
+int DataCollection::fileExists() {
+    return std::filesystem::exists(csvPath);
+}
+
+int DataCollection::createTable(std::shared_ptr<arrow::Table> &table) {
+    return 0;
+}
+
+void DataCollection::displayTable(const std::shared_ptr<arrow::Table> &table) {
+    std::cout << table << std::endl;
+}
+
+int DataCollection::getCsv(std::shared_ptr<arrow::csv::TableReader> &reader) {
+    return 0;
+}
+
+int DataCollection::getRows() {
+    return numRows;
+}
+
+int DataCollection::getColumns() {
+    return numColumns;
+}
+
+std::string DataCollection::getPath() {
+    return csvPath;
+}
+
+void DataCollection::setRows(int &rows) {
+    numRows = rows;
+}
+
+void DataCollection::setColumns(int &columns) {
+    numColumns = columns;
+}
+
+void DataCollection::setPath(std::string_view path) {
+    csvPath = path;
+}
+
+DataCollection::~DataCollection() = default;
--- ml/headers/data.h
+++ ml/headers/data.h
@@ -4,7 +4,9 @@
 #include <string_view>
 #include <arrow/csv/api.h>
 #include <arrow/api.h>
+#include <arrow/csv/reader.h>
 #include <memory>
+#include <filesystem>
 
 class DataCollection {
 public:
@@ -12,9 +14,16 @@ public:
     ~DataCollection();
 protected:
     int createTable(std::shared_ptr<arrow::Table>& table);
-    void displayTable(std::shared_ptr<arrow::Table> table);
+    void displayTable(const std::shared_ptr<arrow::Table>& table);
+    int getRows();
+    int getColumns();
+    std::string getPath();
+    void setRows(int& rows);
+    void setColumns(int& columns);
+    void setPath(std::string_view path);
 private:
-    int getCsv(arrow::Status& status);
+    int getCsv(std::shared_ptr<arrow::csv::TableReader>& reader);
+    int fileExists();
     std::string csvPath;
     int numRows;
     int numColumns;
