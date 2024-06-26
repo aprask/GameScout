4465872c8de9b7015192c9f815c8d54d7a99ae6b
Jacob Praskala <praskalaj@gmail.com>
2024-06-26 17:10:50 -0400

    Finished Data

--- ml/cpp_files/data.cpp
+++ ml/cpp_files/data.cpp
@@ -10,23 +10,53 @@ int DataCollection::fileExists() {
     return std::filesystem::exists(csvPath);
 }
 
-int DataCollection::createTable(std::shared_ptr<arrow::Table> &table) {
-    return 0;
+arrow::Status DataCollection::readCsv(std::shared_ptr<arrow::csv::TableReader> &reader) {
+    std::shared_ptr<arrow::io::ReadableFile> file;
+    ARROW_ASSIGN_OR_RAISE(file, arrow::io::ReadableFile::Open(csvPath));
+    arrow::csv::ReadOptions readOptions;
+    arrow::csv::ParseOptions parseOptions;
+    arrow::csv::ConvertOptions convertOptions;
+
+    ARROW_ASSIGN_OR_RAISE(reader, arrow::csv::TableReader::Make(arrow::io::default_io_context(), file, readOptions, parseOptions, convertOptions));
+
+    return arrow::Status::OK();
+}
+
+arrow::Status DataCollection::createTable(std::shared_ptr<arrow::csv::TableReader> &reader,
+                                std::shared_ptr<arrow::Table> &table) {
+    ARROW_ASSIGN_OR_RAISE(table, reader->Read());
+    return arrow::Status::OK();
+}
+
+int DataCollection::collectData(std::shared_ptr<arrow::Table> &table) {
+    if (!fileExists()) {
+        std::cout << "Couldn't find the path to the csv file." << std::endl;
+        return -1;
+    }
+
+    std::shared_ptr<arrow::csv::TableReader> tableReader;
+    if (!readCsv(tableReader).ok()) {
+        std::cout << "Couldn't read the contents of the csv file." << std::endl;
+        return -1;
+    }
+
+    if (!createTable(tableReader, table).ok()) {
+        std::cout << "Couldn't read the contents of the table reader into the table." << std::endl;
+        return -1;
+    }
+
+    return (int) table->num_rows();
 }
 
 void DataCollection::displayTable(const std::shared_ptr<arrow::Table> &table) {
-    std::cout << table << std::endl;
+    std::cout << table->schema()->ToString() << std::endl;
 }
 
-int DataCollection::getCsv(std::shared_ptr<arrow::csv::TableReader> &reader) {
-    return 0;
-}
-
-int DataCollection::getRows() {
+int DataCollection::getRows() const {
     return numRows;
 }
 
-int DataCollection::getColumns() {
+int DataCollection::getColumns() const {
     return numColumns;
 }
 
--- ml/headers/data.h
+++ ml/headers/data.h
@@ -5,6 +5,7 @@
 #include <arrow/csv/api.h>
 #include <arrow/api.h>
 #include <arrow/csv/reader.h>
+#include <arrow/io/api.h>
 #include <memory>
 #include <filesystem>
 
@@ -13,16 +14,17 @@ public:
     DataCollection();
     ~DataCollection();
 protected:
-    int createTable(std::shared_ptr<arrow::Table>& table);
-    void displayTable(const std::shared_ptr<arrow::Table>& table);
-    int getRows();
-    int getColumns();
+    int collectData(std::shared_ptr<arrow::Table>& table);
+    static void displayTable(const std::shared_ptr<arrow::Table>& table);
+    [[nodiscard]] int getRows() const;
+    [[nodiscard]] int getColumns() const;
     std::string getPath();
     void setRows(int& rows);
     void setColumns(int& columns);
     void setPath(std::string_view path);
 private:
-    int getCsv(std::shared_ptr<arrow::csv::TableReader>& reader);
+    arrow::Status readCsv(std::shared_ptr<arrow::csv::TableReader>& reader);
+    static arrow::Status createTable(std::shared_ptr<arrow::csv::TableReader>& reader, std::shared_ptr<arrow::Table>& table);
     int fileExists();
     std::string csvPath;
     int numRows;
