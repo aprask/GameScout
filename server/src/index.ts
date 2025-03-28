803df757778c875d3104d8d5bda40fe6d55268eb
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-03-28 16:04:20 -0400

    Refactored backend to use express

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import VError from 'verror';
+
+export const app = express();
+app.use(express.json());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    const info = VError.info(err);
+    console.log(err);
+    res.status(info?.statusCode ?? 500).json({ error: info?.response ?? 'Internal Server Error' });
+});
+
+const PORT = process.env.PORT ?? 3000;
+
+if (process.env.APP_ENV !== 'test') {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
+}
