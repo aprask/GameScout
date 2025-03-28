92ac253818e2a9105cf6993625929febd30c0610
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-03-28 16:31:10 -0400

    Added simple homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Typography, Container } from "@mui/material";
+import NavBar from './components/NavBar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <Container sx={{ mt: 4, textAlign: "center" }}>
+        <Typography variant="h3" gutterBottom>
+          Welcome to Gamescout
+        </Typography>
+        <Typography variant="body1">
+          Discover and track your favorite games with ease.
+        </Typography>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Box } from "@mui/system";
+import React from 'react';
+
+function NavBar() {
+    return (
+        <Box>
+            <AppBar position="static">
+                <Toolbar>
+                <SportsEsportsIcon sx={{ mr: 1 }} />
+                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                    Gamescout
+                </Typography>
+                    <Button color="inherit">Dashboard</Button>
+                    <Button color="inherit">Community</Button>
+                    <Button color="inherit">Games</Button>
+                    <Button color="inherit">Profile</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+export default NavBar;
