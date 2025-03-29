7d63ab7c83048db33fddfe0abc70d4ff6cff55ac
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-03-29 18:08:10 -0400

    fixed comment

--- scheduler-proc/job/game-cron.ts
+++ scheduler-proc/job/game-cron.ts
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 
 export default function gameJob() {
   cron.schedule('0 0 18 * * 6',
-     async () => { // this will occur once a week (2 am every Sat) https://www.npmjs.com/package/node-cron?activeTab=readme
+     async () => { // this will occur once a week (6 pm every Sat) https://www.npmjs.com/package/node-cron?activeTab=readme
       console.log(`[${new Date().toISOString()}] Running job`);
     },
     {
