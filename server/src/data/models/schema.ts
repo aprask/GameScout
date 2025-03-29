3bb2f8c539d94bce3f3d3b03c4d0c976f769597c
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-03-28 21:09:13 -0400

    Updated schema model file to include all tables

--- server/src/data/models/schema.ts
+++ server/src/data/models/schema.ts
@@ -1,9 +1,15 @@
-import { UserTable, ReviewTable, WishlistTable, ProfileTable, FollowsTable } from "./models.js";
+import { UserTable, ReviewTable, WishlistTable, ProfileTable, FollowsTable, AdminTable, ImageTable, GameTable, ArticleTable, ArticleCommentTable, AuthTable } from "./models.js";
 
 export interface Schema {
     user: UserTable;
-    game_review: ReviewTable;
+    admin: AdminTable;
+    images: ImageTable;
+    games: GameTable;
+    review: ReviewTable;
     wishlist: WishlistTable;
     profile: ProfileTable;
+    articles: ArticleTable;
+    article_comments: ArticleCommentTable;
+    auth: AuthTable;
     follows: FollowsTable;
   }
