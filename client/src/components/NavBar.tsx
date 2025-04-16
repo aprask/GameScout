1f81098e935b930cb0ccde68d316b1f9f3be6e17
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-04-15 23:24:13 -0400

    Updated nav bar to include avatar that links to a profile page

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -12,11 +12,54 @@ import {
   Avatar,
   useTheme,
 } from "@mui/material";
-import { useState } from "react";
-import { NavLink } from "react-router";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useAuth } from '../context/AuthContext';
 
 function NavBar() {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [profileImage, setProfileImage] = useState<string>("/anon.png");
+  const { isAuthenticated, profileId, token, logout } = useAuth()
+
+  const handleLogout = () => {
+    logout();
+    toggleDrawer();
+  };
+
+  useEffect(() => {
+    if (!isAuthenticated) return;
+    const fetchProfileImg = async() => {
+      try {
+        let res = await axios.get(
+          `http://localhost:3000/api/v1/profile/${profileId}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            }
+          }
+        );
+        res = await axios.get(
+            `http://localhost:3000/api/v1/image/${res.data.profile.profile_img}`,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              }
+            }
+          );
+          const blob = new Blob([res.data.image.image_data.data], { type: 'image/png' });
+          if (blob) {
+            const imageURL = URL.createObjectURL(blob);
+            setProfileImage(imageURL);
+          }
+      } catch (err) {
+        console.error("Error fetching profile image:", err)
+      }
+    }
+    fetchProfileImg();
+  }, [])
 
   const toggleDrawer = () => {
     setDrawerOpen(!drawerOpen);
@@ -37,29 +80,44 @@ function NavBar() {
         }}
       >
         <Toolbar>
-          <IconButton edge="start" color="inherit" onClick={toggleDrawer}>
-            <SportsEsportsIcon />
-          </IconButton>
+          {isAuthenticated && (
+            <IconButton edge="start" color="inherit" onClick={toggleDrawer}>
+              <SportsEsportsIcon />
+            </IconButton>
+          )}
           <Typography variant="h6" noWrap sx={{ flexGrow: 1 }}>
             Gamescout
           </Typography>
-          <Avatar alt="Profile Pic" src="" />
+          {isAuthenticated && (
+            <Box sx={{ width: 100 }}>
+              <NavLink
+                to="/profile"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <Avatar 
+                  alt="Profile Pic" 
+                  src={profileImage}
+                  sx={{ width: 40, height: 40 }}
+                />
+              </NavLink>
+            </Box>
+          )}
         </Toolbar>
       </AppBar>
 
-      <Drawer
-        //variant="persistent"
-        anchor="left"
-        open={drawerOpen}
-        sx={{
-          width: 250,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
+      {isAuthenticated && (
+        <Drawer
+          anchor="left"
+          open={drawerOpen}
+          sx={{
             width: 250,
-            boxSizing: "border-box",
-          },
-        }}
-      >
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: 250,
+              boxSizing: "border-box",
+            },
+          }}
+        >
         <Toolbar />
         <Box sx={{ overflow: "auto" }}>
           <List>
@@ -89,18 +147,18 @@ function NavBar() {
               style={{ textDecoration: "none", color: "inherit" }}
               onClick={toggleDrawer}
             >
-              <ListItemButton>Profile</ListItemButton>
+              <ListItemButton>Articles</ListItemButton>
             </NavLink>
             <NavLink
-              to="/"
+              to="/login"
               style={{ textDecoration: "none", color: "inherit" }}
-              onClick={toggleDrawer}
+              onClick={handleLogout}
             >
-              <ListItemButton>Articles</ListItemButton>
+              <ListItemButton>Logout</ListItemButton>
             </NavLink>
           </List>
         </Box>
-      </Drawer>
+      </Drawer> )}
 
       <Box
         component="main"
