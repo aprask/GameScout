fa260410dc8ed6722f1f1ae6685ec0c07e741881
CalebFilip <filip0732caleb@gmail.com>
2025-04-22 15:32:15 -0400

    fixed cron job schedule

--- scheduler-proc/job/game-cron.ts
+++ scheduler-proc/job/game-cron.ts
@@ -17,7 +17,7 @@ export default async function gameJob() {
   const producer = new Producer("GAME_DATA", "");
   const games: Game[] = [];
   cron.schedule(
-    "18 15 * * *",
+    "0 16 13 * * 1",
     async () => {
       console.log("starting job");
       try {
