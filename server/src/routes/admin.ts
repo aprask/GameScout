ffb7d3f9d1a9ef5add9cd46c7f7186b85a4c5b68
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2025-04-26 20:11:41 -0400

    Removed middleware from admin

--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -6,12 +6,6 @@ import { authMiddleware } from '../middleware/auth.js';
 import express, { RequestHandler } from 'express';
 import { resourceSharer } from '../middleware/resource.js';
 
-router.use((req, res, next) => {
-  console.log("Admin route middleware hit");
-  console.log("Headers:", req.headers);
-  next();
-});
-
 router.use(resourceSharer);
 router.use(authMiddleware as RequestHandler);
 
