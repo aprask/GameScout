d1179dcfa9acc748c0b757ccec31a430640bf82c
CalebFilip <filip0732caleb@gmail.com>
2025-04-29 17:28:13 -0400

    wishlist routes

--- server/src/repository/wishlist.ts
+++ server/src/repository/wishlist.ts
@@ -1,70 +1,71 @@
-import { db } from "../data/db.js";
-import { WishlistTable } from "../data/models/models.js";
-import { throwErrorException } from "../util/error.js";
+import { db } from '../data/db.js';
+import { WishlistTable } from '../data/models/models.js';
+import { throwErrorException } from '../util/error.js';
 
 export async function getAllWishlists(): Promise<WishlistTable[]> {
-    const wishlists = await db
-        .selectFrom("wishlist")
-        .selectAll()
-        .execute();
-    if (wishlists === undefined) throwErrorException(`[repository.wishlist.getAllWishlists] cannot get wishlists`, 'Wishlists is undefined', 404);
-    return wishlists;
+  const wishlists = await db.selectFrom('wishlist').selectAll().execute();
+  if (wishlists === undefined) throwErrorException(`[repository.wishlist.getAllWishlists] cannot get wishlists`, 'Wishlists is undefined', 404);
+  return wishlists;
 }
 
 export async function getWishListsByUserId(user_id: string): Promise<WishlistTable[]> {
-    const wishlists = await db
-        .selectFrom("wishlist")
-        .selectAll()
-        .where('user_id', '=', user_id)
-        .execute();
-    if (wishlists === undefined) throwErrorException(`[repository.wishlist.getAllWishlists] cannot get wishlists`, 'Wishlists is undefined', 404);
-    return wishlists;
+  const wishlists = await db.selectFrom('wishlist').selectAll().where('user_id', '=', user_id).execute();
+  if (wishlists === undefined) throwErrorException(`[repository.wishlist.getAllWishlists] cannot get wishlists`, 'Wishlists is undefined', 404);
+  return wishlists;
 }
 
 export async function getWishlistById(wishlist_id: string): Promise<WishlistTable> {
-    const wishlist = await db
-        .selectFrom("wishlist")
-        .selectAll()
-        .where("wishlist_id", "=", wishlist_id)
-        .executeTakeFirst();
-    if (!wishlist || wishlist === undefined) throwErrorException(`[repository.wishlist.getWishlistById] cannot find wishlist with ID ${wishlist_id}`, 'Wishlist not found', 404);
-    return wishlist!;
+  const wishlist = await db.selectFrom('wishlist').selectAll().where('wishlist_id', '=', wishlist_id).executeTakeFirst();
+  if (!wishlist || wishlist === undefined)
+    throwErrorException(`[repository.wishlist.getWishlistById] cannot find wishlist with ID ${wishlist_id}`, 'Wishlist not found', 404);
+  return wishlist!;
 }
 
 export async function createWishlist(wishlist: WishlistTable): Promise<WishlistTable> {
-    const newWishlist = await db
-        .insertInto("wishlist")
-        .values({
-            wishlist_id: wishlist.wishlist_id,
-            user_id: wishlist.user_id,
-            game_id: wishlist.game_id,
-            created_at: wishlist.created_at,
-            updated_at: wishlist.updated_at,
-        })
-        .returningAll()
-        .executeTakeFirst();
-    if (!newWishlist || newWishlist === undefined) throwErrorException(`[repository.wishlist.createWishlist] cannot create wishlist`, 'Cannot create wishlist', 500);
-    return newWishlist!;
+  const newWishlist = await db
+    .insertInto('wishlist')
+    .values({
+      wishlist_id: wishlist.wishlist_id,
+      user_id: wishlist.user_id,
+      game_id: wishlist.game_id,
+      created_at: wishlist.created_at,
+      updated_at: wishlist.updated_at,
+    })
+    .returningAll()
+    .executeTakeFirst();
+  if (!newWishlist || newWishlist === undefined)
+    throwErrorException(`[repository.wishlist.createWishlist] cannot create wishlist`, 'Cannot create wishlist', 500);
+  return newWishlist!;
 }
 
 export async function updateWishlist(wishlist_id: string, wishlist: Omit<WishlistTable, 'wishlist_id' | 'created_at' | 'updated_at'>): Promise<WishlistTable> {
-    const updatedWishlist = await db
-        .updateTable("wishlist")
-        .set({
-            user_id: wishlist.user_id,
-            game_id: wishlist.game_id,
-            updated_at: new Date()
-        })
-        .where("wishlist_id", "=", wishlist_id)
-        .returningAll()
-        .executeTakeFirst();
-    if (!updatedWishlist || updatedWishlist === undefined) throwErrorException(`[repository.wishlist.updateWishlist] cannot update wishlist`, 'Cannot update wishlist', 500);
-    return updatedWishlist!;
+  const updatedWishlist = await db
+    .updateTable('wishlist')
+    .set({
+      user_id: wishlist.user_id,
+      game_id: wishlist.game_id,
+      updated_at: new Date(),
+    })
+    .where('wishlist_id', '=', wishlist_id)
+    .returningAll()
+    .executeTakeFirst();
+  if (!updatedWishlist || updatedWishlist === undefined)
+    throwErrorException(`[repository.wishlist.updateWishlist] cannot update wishlist`, 'Cannot update wishlist', 500);
+  return updatedWishlist!;
 }
 
 export async function deleteWishlist(wishlist_id: string): Promise<void> {
-    await db
-        .deleteFrom("wishlist")
-        .where("wishlist_id", "=", wishlist_id)
-        .executeTakeFirstOrThrow();
+  await db.deleteFrom('wishlist').where('wishlist_id', '=', wishlist_id).executeTakeFirstOrThrow();
+}
+
+export async function getWishlistByGameAndUserId(game_id: string, user_id: string) {
+  const review = await db.selectFrom('wishlist').selectAll().where('user_id', '=', user_id).where('game_id', '=', game_id).executeTakeFirst();
+  if (!review) {
+    throwErrorException(
+      `[repository.wishlist.getWishlistByGameandUserId] No reviews found for game ID: ${game_id} and user ID: ${user_id}`,
+      'Wishlist not found',
+      200,
+    );
+  }
+  return review;
 }
--- server/src/service/wishlist.ts
+++ server/src/service/wishlist.ts
@@ -1,70 +1,81 @@
-import { WishlistTable } from "../data/models/models.js";
-import * as wishlistRepo from "../repository/wishlist.js";
-import { throwErrorException } from "../util/error.js";
-import { v4 as uuidv4, validate } from "uuid";
-import * as adminRepo from "../repository/admin.js";
+import { WishlistTable } from '../data/models/models.js';
+import * as wishlistRepo from '../repository/wishlist.js';
+import { throwErrorException } from '../util/error.js';
+import { v4 as uuidv4, validate } from 'uuid';
+import * as adminRepo from '../repository/admin.js';
 
 export function getAllWishlists(): Promise<WishlistTable[]> {
-    return wishlistRepo.getAllWishlists();
+  return wishlistRepo.getAllWishlists();
 }
 
 export function getWishListsByUserId(user_id: string): Promise<WishlistTable[]> {
-    if (!validate(user_id)) throwErrorException(`[service.wishlist.getWishListsByUserId] Invalid UUID: ${user_id}`, 'Invalid user ID', 400);
-    return wishlistRepo.getWishListsByUserId(user_id);
+  if (!validate(user_id)) throwErrorException(`[service.wishlist.getWishListsByUserId] Invalid UUID: ${user_id}`, 'Invalid user ID', 400);
+  return wishlistRepo.getWishListsByUserId(user_id);
 }
 
 export async function getWishlistById(wishlist_id: string): Promise<WishlistTable> {
-    if (!validate(wishlist_id)) throwErrorException(`[service.wishlist.getWishlistById] Invalid UUID: ${wishlist_id}`, 'Invalid wishlist ID', 400);
-    return wishlistRepo.getWishlistById(wishlist_id);
+  if (!validate(wishlist_id)) throwErrorException(`[service.wishlist.getWishlistById] Invalid UUID: ${wishlist_id}`, 'Invalid wishlist ID', 400);
+  return wishlistRepo.getWishlistById(wishlist_id);
 }
 
 export async function createWishlist(user_id: string, game_id: string): Promise<WishlistTable> {
-    let errorMessage = '';
-    if (!user_id) errorMessage += "User ID not given. ";
-    if (!game_id) errorMessage += "Game ID not given. ";
-    if (!validate(user_id)) errorMessage += "User ID is invalid. ";
-    if (!validate(game_id)) errorMessage += "Game ID is invalid. ";
-    if (errorMessage) {
-        errorMessage.trim();
-        throwErrorException(`[service.wishlist.createWishlist] ${errorMessage}`, 'Cannot create wishlist', 400);
-    }
+  let errorMessage = '';
+  if (!user_id) errorMessage += 'User ID not given. ';
+  if (!game_id) errorMessage += 'Game ID not given. ';
+  if (!validate(user_id)) errorMessage += 'User ID is invalid. ';
+  if (!validate(game_id)) errorMessage += 'Game ID is invalid. ';
+  if (errorMessage) {
+    errorMessage.trim();
+    throwErrorException(`[service.wishlist.createWishlist] ${errorMessage}`, 'Cannot create wishlist', 400);
+  }
 
-    const currentDate = new Date();
-    const newWishlist: WishlistTable = {
-        wishlist_id: uuidv4(),
-        user_id,
-        game_id,
-        created_at: currentDate,
-        updated_at: currentDate
-    };
+  const currentDate = new Date();
+  const newWishlist: WishlistTable = {
+    wishlist_id: uuidv4(),
+    user_id,
+    game_id,
+    created_at: currentDate,
+    updated_at: currentDate,
+  };
 
-    return wishlistRepo.createWishlist(newWishlist);
+  return wishlistRepo.createWishlist(newWishlist);
 }
 
 export async function updateWishlist(wishlist_id: string, user_id: string, game_id: string): Promise<WishlistTable> {
-    let errorMessage = '';
-    if (!wishlist_id) errorMessage += "Wishlist ID not given";
-    if (!validate(wishlist_id)) errorMessage += "Wishlist ID is invalid";
-    if (errorMessage) {
-        errorMessage.trim();
-        throwErrorException(`[service.wishlist.updateWishlist] ${errorMessage}`, 'Cannot update wishlist', 400);
-    }
+  let errorMessage = '';
+  if (!wishlist_id) errorMessage += 'Wishlist ID not given';
+  if (!validate(wishlist_id)) errorMessage += 'Wishlist ID is invalid';
+  if (errorMessage) {
+    errorMessage.trim();
+    throwErrorException(`[service.wishlist.updateWishlist] ${errorMessage}`, 'Cannot update wishlist', 400);
+  }
 
-    const currentWishlist = await wishlistRepo.getWishlistById(wishlist_id);
-    const updatedWishlist: Omit<WishlistTable, 'wishlist_id' | 'created_at' | 'updated_at'> = {
-        user_id: user_id ?? currentWishlist.user_id,
-        game_id: game_id ?? currentWishlist.game_id
-    };
+  const currentWishlist = await wishlistRepo.getWishlistById(wishlist_id);
+  const updatedWishlist: Omit<WishlistTable, 'wishlist_id' | 'created_at' | 'updated_at'> = {
+    user_id: user_id ?? currentWishlist.user_id,
+    game_id: game_id ?? currentWishlist.game_id,
+  };
 
-    return wishlistRepo.updateWishlist(wishlist_id, updatedWishlist);
+  return wishlistRepo.updateWishlist(wishlist_id, updatedWishlist);
 }
 
-export async function deleteWishlist(wishlist_id: string, admin_id: string): Promise<void> {
-    if (admin_id && wishlist_id) {
-        if (!validate(admin_id)) throwErrorException(`[service.wishlist.deleteWishlist] Invalid UUID: ${admin_id}`, 'Invalid admin ID', 400);
-        if (!(await adminRepo.getAdminById(admin_id))) throwErrorException(`[service.wishlist.deleteWishlist] Admin ID invalid: ${admin_id}`, 'Admin ID invalid', 400);
-        if (!validate(wishlist_id)) throwErrorException(`[service.wishlist.deleteWishlist] Invalid UUID: ${wishlist_id}`, 'Invalid wishlist ID', 400);
-        else wishlistRepo.deleteWishlist(wishlist_id);
-    }
-    else throwErrorException(`[service.wishlist.deleteWishlist] No valid ID providded`, 'Cannot delete wishlist', 403);
+export async function deleteWishlist(wishlist_id: string, admin_id: string | null, user_id: string | null): Promise<void> {
+  if (admin_id && wishlist_id) {
+    if (!validate(admin_id)) throwErrorException(`[service.wishlist.deleteWishlist] Invalid UUID: ${admin_id}`, 'Invalid admin ID', 400);
+    if (!(await adminRepo.getAdminById(admin_id)))
+      throwErrorException(`[service.wishlist.deleteWishlist] Admin ID invalid: ${admin_id}`, 'Admin ID invalid', 400);
+    if (!validate(wishlist_id)) throwErrorException(`[service.wishlist.deleteWishlist] Invalid UUID: ${wishlist_id}`, 'Invalid wishlist ID', 400);
+    else wishlistRepo.deleteWishlist(wishlist_id);
+  }
+  if (user_id && user_id) {
+    if (!validate(user_id)) throwErrorException(`[service.wishlist.deleteWishlist] Invalid UUID: ${user_id}`, 'Invalid user ID', 400);
+    if (!validate(wishlist_id)) throwErrorException(`[service.wishlist.deleteWishlist] Invalid UUID: ${wishlist_id}`, 'Invalid wishlist ID', 400);
+    else wishlistRepo.deleteWishlist(wishlist_id);
+  } else throwErrorException(`[service.wishlist.deleteWishlist] No valid ID providded`, 'Cannot delete wishlist', 403);
+}
+
+export async function getWishlistByGameAndUserId(game_id: string, user_id: string): Promise<WishlistTable | null> {
+  if (!user_id) throwErrorException(`[service.review.getReviewByGameAndUserId] No user_id provided`, 'user_id', 404);
+  if (!game_id) throwErrorException(`[service.review.getReviewByGameAndUserId] No game_id provided`, 'game_id', 404);
+  else return wishlistRepo.getWishlistByGameAndUserId(game_id, user_id);
 }
