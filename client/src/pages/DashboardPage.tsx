940c75622ef7a56c70949cac8647cf42f1044812
CalebFilip <filip0732caleb@gmail.com>
2025-04-30 21:26:57 -0400

    added about us and profile link

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,9 +1,11 @@
 import {
+  Avatar,
   Box,
   Card,
   CardContent,
   Container,
   CssBaseline,
+  IconButton,
   ImageList,
   ImageListItem,
   Paper,
@@ -11,7 +13,13 @@ import {
 } from "@mui/material";
 import { JSX, useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import caleb from "../../public/caleb.jpg";
+import andrew from "../../public/andrew.jpg";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { useProfile } from "../context/profile/ProfileContext";
+import { useAuth } from "../context/auth/AuthContext";
 
 interface GameData {
   created_at: Date;
@@ -37,6 +45,8 @@ interface ArticleData {
 function DashboardPage(): JSX.Element {
   const [newGames, setNewGames] = useState<GameData[] | null>();
   const [newArticles, setNewArticles] = useState<ArticleData[] | null>();
+  const { profileImage } = useProfile();
+  const { profileId } = useAuth();
   const navigate = useNavigate();
 
   const baseUrl =
@@ -115,11 +125,18 @@ function DashboardPage(): JSX.Element {
                 </CardContent>
               )}
             </Card>
-            <Card sx={{ m: 5, ml: 2 }}>
-              <CardContent>
-                <Typography variant="h5">Profile</Typography>
-              </CardContent>
-            </Card>
+            <Box>
+              <NavLink
+                to={`/profile/${profileId}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <Avatar
+                  alt="Profile Pic"
+                  src={profileImage || undefined}
+                  sx={{ mt: 10, ml: 10, height: "60%", width: "60%" }}
+                />
+              </NavLink>
+            </Box>
           </Box>
           {newArticles && (
             <Card sx={{ m: 5, mt: 0 }}>
@@ -128,6 +145,7 @@ function DashboardPage(): JSX.Element {
 
                 {newArticles!.map((article) => (
                   <Paper
+                    key={article.article_id}
                     elevation={3}
                     sx={{ m: 1, p: 1 }}
                     onClick={() =>
@@ -143,6 +161,124 @@ function DashboardPage(): JSX.Element {
           <Card sx={{ m: 5 }}>
             <CardContent>
               <Typography variant="h5">About Us</Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  gap: 2,
+                  mt: 2,
+                }}
+              >
+                <Box
+                  sx={{
+                    width: "50%",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <img
+                    src={andrew}
+                    alt="Andrew"
+                    style={{
+                      width: "150px",
+                      height: "150px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                  <Paper elevation={3} sx={{ m: 2, p: 2 }}>
+                    <Typography variant="h5">Andrew Praskala</Typography>
+                    <Typography variant="body1">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Donec bibendum orci non leo venenatis rutrum. Suspendisse
+                      iaculis, massa at suscipit convallis, eros nisi tristique
+                      tortor, ac bibendum nisi sapien at lorem. Quisque lacinia
+                      lectus vel suscipit laoreet. Quisque dignissim ex quis
+                      nulla malesuada, feugiat mattis enim convallis. In sed
+                      lectus imperdiet, aliquet ex vitae, sagittis erat. Cras
+                      ullamcorper sit amet augue at egestas. Duis pellentesque
+                      felis vitae tristique congue.
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-evenly",
+                        mt: 1,
+                      }}
+                    >
+                      <IconButton
+                        aria-label="GitHub"
+                        href="https://github.com/aprask"
+                      >
+                        <GitHubIcon sx={{ fontSize: 45 }} />
+                      </IconButton>
+                      <IconButton
+                        aria-label="LinkedIn"
+                        href="https://www.linkedin.com/in/andrewpraskala/"
+                      >
+                        <LinkedInIcon sx={{ fontSize: 45 }} />
+                      </IconButton>
+                    </Box>
+                  </Paper>
+                </Box>
+                <Box
+                  sx={{
+                    width: "50%",
+                    height: "100%",
+
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <img
+                    src={caleb}
+                    alt="Caleb"
+                    style={{
+                      width: "150px",
+                      height: "150px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                  <Paper elevation={3} sx={{ m: 2, p: 2 }}>
+                    <Typography variant="h5">Caleb Filip</Typography>
+                    <Typography variant="body1">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Donec bibendum orci non leo venenatis rutrum. Suspendisse
+                      iaculis, massa at suscipit convallis, eros nisi tristique
+                      tortor, ac bibendum nisi sapien at lorem. Quisque lacinia
+                      lectus vel suscipit laoreet. Quisque dignissim ex quis
+                      nulla malesuada, feugiat mattis enim convallis. In sed
+                      lectus imperdiet, aliquet ex vitae, sagittis erat. Cras
+                      ullamcorper sit amet augue at egestas. Duis pellentesque
+                      felis vitae tristique congue.
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-evenly",
+                        mt: 1,
+                      }}
+                    >
+                      <IconButton
+                        aria-label="GitHub"
+                        href="https://github.com/CalebFilip"
+                      >
+                        <GitHubIcon sx={{ fontSize: 45 }} />
+                      </IconButton>
+                      <IconButton
+                        aria-label="LinkedIn"
+                        href="https://www.linkedin.com/in/calebfilip/"
+                      >
+                        <LinkedInIcon sx={{ fontSize: 45 }} />
+                      </IconButton>
+                    </Box>
+                  </Paper>
+                </Box>
+              </Box>
             </CardContent>
           </Card>
         </Box>
