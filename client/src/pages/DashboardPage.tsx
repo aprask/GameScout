13820ff634bff0964acef89807ce18e1fac2b500
CalebFilip <filip0732caleb@gmail.com>
2025-04-30 20:06:14 -0400

    bug fixes

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -7,9 +7,7 @@ import {
   ImageList,
   ImageListItem,
   Typography,
-  //useTheme,
 } from "@mui/material";
-import image from "../assets/image.png";
 import { JSX, useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
